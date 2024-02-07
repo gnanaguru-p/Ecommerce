618def0daef8c41e78b1a34fc6d8c726b7926c25
Guru8991 <gnanaguru8991@gmail.com>
2024-02-07 14:39:25 +0530

    last commit

--- server/config/connectDatabase.js
+++ server/config/connectDatabase.js
@@ -4,8 +4,10 @@ const connectDatabase = () => {
     mongoose.connect(process.env.DB_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        retryWrites: true // or false depending on your preference
-    }).then(con => {
+        retryWrites: true, // or false depending on your preference
+        // Increase timeout value
+        serverSelectionTimeoutMS: 30000, // 30 seconds
+      }).then(con => {
         console.log(`MongoDB is connected to the host: ${con.connection.host}`);
     }).catch(err => {
         console.error('Error connecting to MongoDB:', err);
