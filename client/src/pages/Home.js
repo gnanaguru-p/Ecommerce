847aa5e214dab28f410429be946864e01014636d
Guru8991 <gnanaguru8991@gmail.com>
2024-02-07 14:50:36 +0530

    last commit

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,54 +1,24 @@
-import React, { Fragment, useEffect, useState } from 'react';
-import ProductCard from '../components/ProductCard';
+import {Fragment, useEffect, useState} from 'react'
+import ProductCard from '../components/ProductCard'
 import { useSearchParams } from 'react-router-dom';
-import Loader from '../components/Loader';
 
 export default function Home() {
     const [products, setProducts] = useState([]);
-    const [searchParams, setSearchParams] = useSearchParams();
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+    const [searchParams, setSearchParams] =  useSearchParams()
 
     useEffect(() => {
-        const fetchData = async () => {
-            setLoading(true); // Set loading state to true when starting the fetch
-            setError(null); // Reset error state
+        fetch(process.env.REACT_APP_API_URL+'/products?'+searchParams)
+        .then(res => res.json())
+        .then( res => setProducts(res.products))
+    },[searchParams])
 
-            try {
-                const response = await fetch(`${process.env.REACT_APP_API_URL}/products?${searchParams}`);
-                if (!response.ok) {
-                    throw new Error('Failed to fetch products');
-                }
-                const data = await response.json();
-                setProducts(data.products);
-            } catch (error) {
-                setError(error.message); // Set error state if fetch fails
-            } finally {
-                setLoading(false); // Set loading state to false after fetch completes
-            }
-        };
+    return <Fragment>
+        <h1 id="products_heading">Latest Products</h1>
 
-        fetchData();
-    }, [searchParams]);
-
-    return (
-        <Fragment>
-            <h1 id="products_heading">Latest Products</h1>
-            {loading ? (
-                <Loader />
-            ) : error ? (
-                <div>Error: {error}</div>
-            ) : products.length === 0 ? (
-                <div>No products found</div>
-            ) : (
-                <section id="products" className="container mt-5">
-                    <div className="row">
-                        {products.map(product => (
-                            <ProductCard key={product._id} product={product} />
-                        ))}
-                    </div>
-                </section>
-            )}
-        </Fragment>
-    );
+        <section id="products" className="container mt-5">
+        <div className="row">
+            {products.map(product =><ProductCard product={product} />)} 
+        </div>
+        </section>
+    </Fragment>
 }
